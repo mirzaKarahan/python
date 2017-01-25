1d65defe3d29fd75bc643f9af49a2c1a8add2354
mirzaKarahan <mirzaakarahan@gmail.com>
2017-01-25 02:07:04 +0200

    1000 e kadar olan tau sayılarının adeti

--- tauSayisi.py
+++ tauSayisi.py
@@ -1,10 +1,14 @@
-sayi = input("Sayıyı Giriniz") # 24 örnek bir Tau sayısıdır
-adet = 0
+tauSayilari = 0
+for sayi in range(1,1000):
+    adet = 0
 
-for i in range(sayi,0,-1):
-    if sayi % i == 0:
-        adet += 1
-if sayi % adet == 0:
-    print(sayi,"Sayısı Bir Tau sayısıdr")
-else:
-    print("!",sayi,"Sayısı Bir Tau Sayısı Değildir !")
+    for i in range(sayi,0,-1):
+        if sayi % i == 0:
+            adet += 1
+    if sayi % adet == 0:
+        print(sayi,"Sayısı Bir Tau sayısıdr")
+        tauSayilari += 1
+    else:
+        print("!",sayi,"Sayısı Bir Tau Sayısı Değildir !")
+
+print(tauSayilari)
